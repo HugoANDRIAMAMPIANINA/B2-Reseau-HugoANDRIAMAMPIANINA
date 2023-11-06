b4a730db0b43aedd5b452f54aeacf49e796b60bc
ahugo <hugo.andriamampianina@ynov.com>
2023-11-06 10:06:12 +0100

    update

---
+++ tp4/bs_client_I1.py
@@ -0,0 +1,19 @@
+import socket
+from sys import exit
+
+host = '10.1.1.11'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((host, port))
+
+s.sendall(b'Meooooo !')
+
+data = s.recv(1024)
+
+print(data)
+
+s.close()
+
+exit(0)
---
+++ tp4/bs_server_I1.py
@@ -0,0 +1,29 @@
+import socket
+
+host = '10.1.1.11'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((host, port))
+
+s.listen(1)
+
+conn, addr = s.accept()
+
+print('Hi mate!')
+
+while True:
+
+    try:
+        data = conn.recv(1024)
+
+        if not data: break
+
+        print(f"{data}")
+
+    except socket.error:
+        print("Error Occured.")
+        break
+
+conn.close()
