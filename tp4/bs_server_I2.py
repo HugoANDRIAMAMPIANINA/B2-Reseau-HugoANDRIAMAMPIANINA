457be631caabd94243a04c7b9403fdcb32bff9c7
ahugo <hugo.andriamampianina@ynov.com>
2023-11-06 11:37:35 +0100

    update

---
+++ tp4/bs_server_I2.py
@@ -0,0 +1,33 @@
+import socket
+from sys import exit
+
+host = '10.1.1.11'
+port = 13337
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.bind((host, port))
+
+s.listen(1)
+
+conn, addr = s.accept()
+
+print(f"Un client vient de se co et son IP c'est {addr[1]}")
+
+conn.sendall(b'Hi mate!')
+
+while True:
+
+    try:
+        data = conn.recv(1024)
+        
+        if not data: break
+
+        print(f"{data.decode()}")
+
+    except socket.error:
+        print("Error Occured.")
+        break
+
+conn.close()
+exit(0)
