d7d6e8dd8eba8f1cba4f057a7708d58e94af3227
ahugo <hugo.andriamampianina@ynov.com>
2023-11-07 11:22:20 +0100

    update

--- tp4/bs_server_II2A.py
+++ tp4/bs_server_II2A.py
@@ -65,7 +65,7 @@ last_client_timer.start()
 
 while True:
     
-    if last_client_timer.finished.is_set:
+    if last_client_timer.finished.is_set():
         print('lala')
         last_client_timer.cancel()
         last_client_timer = Timer(60,timeout)
