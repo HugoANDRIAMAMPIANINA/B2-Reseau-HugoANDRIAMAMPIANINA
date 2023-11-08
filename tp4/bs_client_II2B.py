0bff42191a31ae85556511ed48d1a6790c7162a1
ahugo <hugo.andriamampianina@ynov.com>
2023-11-08 18:45:07 +0100

    update

--- tp4/bs_client_II2B.py
+++ tp4/bs_client_II2B.py
@@ -12,9 +12,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 info_logger = logging.getLogger("info_logger")
 info_logger.setLevel(logging.INFO)
 
-# error_logger = logging.getLogger("error_logger")
-# error_logger.setLevel(logging.ERROR)
-
 color_formatter = ColoredFormatter('%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 error_color_formatter = ColoredFormatter('%(levelname)s %(message)s')
 
@@ -22,20 +19,12 @@ log_file_path = '/var/log/bs_client/bs_client.log'
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setFormatter(color_formatter)
 
-# error_file_handler = logging.FileHandler(log_file_path)
-# error_file_handler.setFormatter(error_color_formatter)
-
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(error_color_formatter)
 console_handler.setLevel(logging.ERROR)
 
 info_logger.addHandler(file_handler)
 info_logger.addHandler(console_handler)
-
-# error_logger.addHandler(error_color_formatter)
-# error_logger.addHandler(error_file_handler)
-
-info_logger.info(f'Le serveur tourne sur {host}:{port}')
 
 try:
     s.connect((host, port))
